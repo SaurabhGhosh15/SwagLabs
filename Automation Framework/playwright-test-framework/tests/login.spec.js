a7aaaa87faa7d1f47e41e69b32d592fafdaaa523
Saurabh Ghosh <saurabh.ghosh15@gmail.com>
2025-02-05 08:18:23 +0530

    Implemented beforeEach in login.spec.js

--- Automation Framework/playwright-test-framework/tests/login.spec.js
+++ Automation Framework/playwright-test-framework/tests/login.spec.js
@@ -3,15 +3,19 @@ import LoginPage from '../pages/login.page';
 //const LoginPage = require('../pages/login.page');
 
 test.describe('Login tests', ()=>{
-    test('Login with valid credentials', async ({page})=>{
-        const loginPage = new LoginPage(page);
+
+let loginPage;
+test.beforeEach('', async({page})=>{
+    loginPage = new LoginPage(page);
+})
+
+    test('Login with valid credentials', async ({page})=>{  
         await loginPage.navigate();
         await loginPage.login('standard_user', 'secret_sauce');
-        expect(await page.url()).toBe('https://www.saucedemo.com/inventory.html');
+        expect(page.url()).toBe('https://www.saucedemo.com/inventory.html');
     });
 
     test('Login with locked credentials', async({page})=>{
-        const loginPage =  new LoginPage(page);
         await loginPage.navigate();
         await loginPage.login('locked_out_user','secret_sauce');
         const errorText = await page.locator('[data-test="error"]').textContent();
